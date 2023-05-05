956900bfe75dc3a11d857036a79eb1f1efd7e692
Herkamal <herkamalrandhawa@gmail.com>
2023-05-05 10:21:06 -0400

    featured books

--- src/components/featured.jsx
+++ src/components/featured.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import Book from "./Book";
+import { books } from "../data";
 
 const Featured = () => {
-  return(
-    <div>
-      featured'
-    </div>
+  console.log(books);
+  console.log(books.filter((book) => book.rating === 5).slice(0, 4));
+  return (
+    <section id="features">
+      <div className="container">
+        <div className="row">
+          <h2 className="section__title">
+            Featured <span className="purple">Books</span>
+          </h2>
+          <div className="books">
+            {books
+            .filter((book) => book.rating === 5)
+            .slice(0, 4)
+            .map((book) => (<Book book={book} key={book.id}></Book>))
+            }
+          </div>
+        </div>
+      </div>
+    </section>
   );
-} 
+};
 
 export default Featured;
