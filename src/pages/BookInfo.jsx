dcdee0813f876b16cfba732e783f5ee8e605933d
Herkamal <herkamalrandhawa@gmail.com>
2023-05-12 12:01:51 -0400

    5 star rec

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Rating from "../components/ui/Rating";
 import Price from "../components/ui/Price";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import Book from "../components/ui/Book";
 
 const BookInfo = ({ books }) => {
   const {id} = useParams()
@@ -66,6 +67,14 @@ const BookInfo = ({ books }) => {
               <h2 className="book__selected--title--top">
                 Recommended Books
               </h2>
+            </div>
+            <div className="books">
+
+            {
+              books.filter((book) => book.rating === 5 && +book.id !== +id)
+              .slice(0,4)
+              .map(book => <Book book={book} key={book.id}></Book>)
+            }
             </div>
           </div>
         </div>
